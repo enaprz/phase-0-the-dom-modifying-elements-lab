1d8683a2cd0fff02cd8d84d93c756d117df24da9
enaprz <enasheccid7@gmail.com>
2023-10-20 20:18:06 -0700

    dom modifying lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+main.remove()
+
+var newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+var body = document.querySelector('body');
+
+
+newHeader.textContent= "Ena is the champion";
+document.body.append(newHeader);
